8e7af5c5eeff11a569cb649399f8e7a17ada1256
Bruno Philippe <bruno.apolinario@trust.com.br>
2019-04-11 17:38:41 -0300

    conecao com mais usuarios

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,9 +1,9 @@
 import io, { Server } from 'socket.io';
-import { Server as HttpServer, IncomingHttpHeaders } from 'http';
+import { Server as HttpServer } from 'http';
 
 export class Socket {
   private server: Server;
-  private rooms: any[] = [];
+  private mapping: Map<String, string> = new Map();
 
   constructor(server: HttpServer) {
     this.server = io(server);
@@ -11,36 +11,25 @@ export class Socket {
   }
 
   inicialize() {
-    const that = this;
     this.server.on('connect', socket => {
       const userId = socket.handshake.query['userId'];
-      socket.join(userId);
 
-      console.log(userId);
+      this.mapping.set(userId, socket.id);
 
-      that.rooms.push(userId);
-      socket.on('send', function(data) {
-        console.log('Convidado:', data.to);
-        console.log('User:', userId);
-        console.log('Usuarios na sala:', that.rooms);
-        socket.to(data.to).emit('messages', {
-          message: data.data,
-          from: data.from
+      socket.on('send', data => {
+        const users = data.to.split(',');
+        users.forEach((user: string) => {
+          socket.to(<string>this.mapping.get(user)).emit('messages', {
+            message: data.data,
+            from: data.from
+          });
         });
+
         socket.emit('self', {
           message: data.data,
           from: userId
         });
-
-        // that.rooms.map(i => {
-        //   console.log('Usuarios na sala:', that.rooms);
-
-        //   });
       });
-
-      this.server.sockets.emit('hi', 'everyone');
     });
   }
-
-  listen() {}
 }
