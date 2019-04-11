ab16a8dd342df071de6c5aa7d80d0cec0bb6fcbe
Bruno Philippe <bruni.felipe@hotmail.com>
2019-04-10 22:53:06 -0300

    integration of two users sending messages beetween they

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -8,7 +8,6 @@ export class Socket {
   constructor(server: HttpServer) {
     this.server = io(server);
     this.inicialize();
-    console.log('here');
   }
 
   inicialize() {
@@ -16,16 +15,27 @@ export class Socket {
     this.server.on('connect', socket => {
       const userId = socket.handshake.query['userId'];
       socket.join(userId);
-      console.log('asd');
+
+      console.log(userId);
+
       that.rooms.push(userId);
-      that.rooms.map(i => {
-        socket.on('message', function(data) {
-          console.log(data);
-          socket.rooms;
-          socket.to(i.to).emit('message', {
-            data: data
-          });
+      socket.on('send', function(data) {
+        console.log('Convidado:', data.to);
+        console.log('User:', userId);
+        console.log('Usuarios na sala:', that.rooms);
+        socket.to(data.to).emit('messages', {
+          message: data.data,
+          from: data.from
         });
+        socket.emit('self', {
+          message: data.data,
+          from: userId
+        });
+
+        // that.rooms.map(i => {
+        //   console.log('Usuarios na sala:', that.rooms);
+
+        //   });
       });
 
       this.server.sockets.emit('hi', 'everyone');
